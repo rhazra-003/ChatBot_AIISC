bb3b2dbdaed81c686f83e5df9fb79b3c54e5278d
Ridam Hazra <rhazra0602@gmail.com>
2021-12-17 11:47:36 +0530

    Add files via upload

---
+++ Final Work/randomiser.py
@@ -0,0 +1,15 @@
+from random import *
+
+intent = input("Enter intent : ")
+
+x = random()
+
+print(intent)
+
+if(intent=='hello'):
+    print('bye')    
+
+if(x<=.7):
+    print("Text Answer")
+else:
+    print("Placebo Answer")
